81eb67fd0b94a701fa57a21fd39990e6f289b79f
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-21 15:05:23 +0600

    validate login form with react hook form

--- src/app/(auth)/login/components/LoginForm/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm/LoginForm.tsx
@@ -1,30 +1,44 @@
 "use client"
 import React from 'react';
 import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type TLoginInputs = {
+    email: string;
+    password: string;
+}
 
 const LoginForm: FC = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<TLoginInputs>()
 
-    const handleLogin = (e: React.FormEvent) => {
-        e.preventDefault();
-    }
+
+    const onSubmit: SubmitHandler<TLoginInputs> = (data) => {
+        console.log(data);
+    };
 
     return (
 
-        <form onSubmit={handleLogin} className='my-6 space-y-3'>
-            <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder='Email address'
-                className='w-full px-4 py-2 border outline-none rounded-md placeholder:text-black/60'
-            />
-            <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder='Password'
-                className='w-full px-4 py-2 border outline-none rounded-md placeholder:text-black/60'
-            />
+        <form onSubmit={handleSubmit(onSubmit)} className='my-6 space-y-3'>
+            <div>
+                <input
+                    type="email"
+                    id="email"
+                    {...register('email', { required: true })}
+                    placeholder='Email address'
+                    className='w-full px-4 py-2 border outline-none rounded-md placeholder:text-black/60'
+                />
+                {errors.email && <p className='text-red-600 text-sm mx-2 my-1'>Email is required</p>}
+            </div>
+            <div>
+                <input
+                    type="password"
+                    {...register('password', { required: true })}
+                    id="password"
+                    placeholder='Password'
+                    className='w-full px-4 py-2 border outline-none rounded-md placeholder:text-black/60'
+                />
+                {errors.password && <p className='text-red-600 text-sm mx-2 my-1'>Password is required</p>}
+            </div>
             <button
                 type='submit'
                 className='w-full px-4 py-2 bg-black hover:bg-black/80 text-white rounded-md my-6 font-medium duration-100'
