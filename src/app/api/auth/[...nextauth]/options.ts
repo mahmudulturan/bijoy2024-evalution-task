40607c800200e0aceff806c481313785ba7d0bdb
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-08-21 15:24:48 +0600

    fix: issues on google login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,8 +3,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const options = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+            clientId: "256561352306-d48la4ebi27s17bkqdmti82bop87mjl8.apps.googleusercontent.com",
+            clientSecret: "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"
         })
     ]
 }
